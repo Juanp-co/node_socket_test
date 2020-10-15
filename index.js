2b640dcadd761af28176ea3b056b7c16905e4cfa
Juan Sebastián Piñeros Hurta <juanmileco@gmail.com>
2020-10-15 16:51:47 -0500

    .

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-app.use(cors())
 const port = 4000
-
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
+app.use(cors())
 
 io.on('connection', (socket) => {
     console.log('a user connected');
