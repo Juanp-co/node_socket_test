32958bd47c5a4a968af382dcc08687d8d6abde9e
Juan Sebastián Piñeros Hurta <juanmileco@gmail.com>
2020-10-19 06:47:28 -0500

    .

--- index.js
+++ index.js
@@ -2,24 +2,18 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 4000
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
- });
-
-app.get('/', function(req, res, next) {
-  // Handle the get for this route
-});
-
-app.post('/', function(req, res, next) {
- // Handle the post for this route
 });
 app.use(cors())
+const port = 4000
+
+var http = require('http').createServer(app);
+var io = require('socket.io')(http,{ origins: '*:*'});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
